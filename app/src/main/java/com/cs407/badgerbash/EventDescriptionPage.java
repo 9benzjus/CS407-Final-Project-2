a15e8662fb484d5df068c4925547acadccce7ccd
Zihao Ye <tonyyezihao@gmail.com>
2023-11-12 17:57:07 -0600

    Event signup page front end somewhat done

---
+++ app/src/main/java/com/cs407/badgerbash/EventDescriptionPage.java
@@ -0,0 +1,14 @@
+package com.cs407.badgerbash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EventDescriptionPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_description_page);
+    }
+}
