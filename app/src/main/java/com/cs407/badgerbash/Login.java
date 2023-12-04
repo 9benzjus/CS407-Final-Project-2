42b06e007ce0774fd1cb93a2cee49a66b76a6bce
Jelly <ryuan28@wisc.edu>
2023-12-03 23:26:52 -0600

    update,basic login works,add log out button in main page

--- app/src/main/java/com/cs407/badgerbash/Login.java
+++ app/src/main/java/com/cs407/badgerbash/Login.java
@@ -3,6 +3,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -63,6 +64,10 @@ public class Login extends AppCompatActivity {
                 String email, password;
                 email = String.valueOf(editTextEmail.getText());
                 password = String.valueOf(editTextPassword.getText());
+                SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString("Username", email);
+                editor.apply(); // Apply the changes to SharedPreferences
 
                 if (TextUtils.isEmpty(email)) {
                     Toast.makeText(Login.this, "Enter email", Toast.LENGTH_SHORT).show();
